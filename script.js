8c94a895998eab0194ae6f1cc09c67de27bc9ca7
AnonymousOtter <33304159+AnonymousOtter@users.noreply.github.com>
2017-11-06 20:22:27 +1300

    Create, Login, Menu

---
+++ script.js
@@ -0,0 +1,47 @@
+function openMenu(evt, item) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(item).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+document.getElementById("defaultOpen").click();
+function openCreate(evt, item) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("createBurgercontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(item).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+
+
+var signIn = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == signIn) {
+        modal.style.display = "none";
+    }
+}
+
+var createBurger = document.getElementById('id02');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == createBurger) {
+        modal.style.display = "none";
+    }
+}
