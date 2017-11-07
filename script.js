4d9ee73fc10d91382fba54c0703e28a0d1ee25ce
AnonymousOtter <33304159+AnonymousOtter@users.noreply.github.com>
2017-11-07 13:41:28 +1300

    Server Attempt

--- script.js
+++ script.js
@@ -11,7 +11,24 @@ function openMenu(evt, item) {
     document.getElementById(item).style.display = "block";
     evt.currentTarget.className += " active";
 }
-document.getElementsById("default").trigger('click');
+// document.getElementsById("default").trigger('click');
+
+
+function openHeader(evt, item) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(item).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+
 
 
 function openCreate(evt, item) {
@@ -46,4 +63,28 @@ window.onclick = function(event) {
     if (event.target == createBurger) {
         modal.style.display = "none";
     }
+}
+
+function login(form){
+  // form.preventDefault;
+  // form.returnValue = false;
+
+  // console.log("login submit");
+  // var http = new XMLHttpRequest();
+  // var url = "http://10.140.124.121/iceberger_backend/login.php";
+  // var name = document.getElementById("logUname").value;
+  // var password = document.getElementById("logPsw").value;
+  // var params = "email=" + name + "&password=" + password;
+  // http.open("POST", url, true);
+  //
+  // http.sendRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  //
+  // http.onreadystatechange = function() {
+  //   if(http.readyState == 4 && http.status == 200){
+  //     console.log(http.responseText);
+  //   }
+  // }
+  // http.send(params);
+
+  // return false;
 }
