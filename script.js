4dec999914e1637acb1c205e224abf38ea6368e7
AnonymousOtter <33304159+AnonymousOtter@users.noreply.github.com>
2017-11-07 16:22:02 +1300

    Looking real good

--- script.js
+++ script.js
@@ -1,48 +1,47 @@
 function openMenu(evt, item) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(item).style.display = "block";
-    evt.currentTarget.className += " active";
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(item).style.display = "block";
+  evt.currentTarget.className += " active";
 }
-// document.getElementsById("default").trigger('click');
 
 
 function openHeader(evt, item) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(item).style.display = "block";
-    evt.currentTarget.className += " active";
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(item).style.display = "block";
+  evt.currentTarget.className += " active";
 }
 
 
 
 
 function openCreate(evt, item) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("createBurgercontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(item).style.display = "block";
-    evt.currentTarget.className += " active";
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("createBurgercontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(item).style.display = "block";
+  evt.currentTarget.className += " active";
 }
 
 
@@ -51,29 +50,48 @@ var signIn = document.getElementById('id01');
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-    if (event.target == signIn) {
-        modal.style.display = "none";
-    }
+  if (event.target == signIn) {
+    modal.style.display = "none";
+  }
 }
 
 var createBurger = document.getElementById('id02');
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-    if (event.target == createBurger) {
-        modal.style.display = "none";
-    }
+  if (event.target == createBurger) {
+    modal.style.display = "none";
+  }
 }
 
 $(document).ready(function() {
-  $("#loginform").submit(function (event) {
+openMenu(event, 'Burgers');
+
+  // document.getElementsById("default").trigger('click');
+  $("#loginform").submit(function(event) {
     event.preventDefault();
 
     var name = document.getElementById("logUname").value;
     var password = document.getElementById("logPsw").value;
     $.getJSON("//10.140.124.121/iceberger_backend/login.php?callback=?", "email=" + name + "&password=" + password, function(data) {
       console.log(data);
-      if(data['success']) {
+      if (data['success']) {
+        $("#id01").hide();
+      }
+    });
+
+    return false;
+  });
+
+  $("#signupform").submit(function(event) {
+    event.preventDefault();
+
+    var name = document.getElementById("signUname").value;
+    var password = document.getElementById("signPsw").value;
+    var email = document.getElementById("signEmail").value;
+    $.getJSON("//10.140.124.121/iceberger_backend/register.php?callback=?", "name=" + name + "&password=" + password + "&email=" + email, function(data) {
+      console.log(data);
+      if (data['success']) {
         $("#id01").hide();
       }
     });
@@ -82,10 +100,10 @@ $(document).ready(function() {
   });
 });
 
-function login(){
+function login() {
 
 }
 
-function logout(){
+function logout() {
 
 }
