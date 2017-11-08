63b94e4299486c87357937cd85f2b2efe053c4c0
AnonymousOtter <33304159+AnonymousOtter@users.noreply.github.com>
2017-11-08 15:18:37 +1300

    Ready for popuation

--- script.js
+++ script.js
@@ -9,7 +9,6 @@ function openMenu(evt, item) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
   document.getElementById(item).style.display = "block";
-  evt.currentTarget.className += " active";
 }
 
 
@@ -65,9 +64,8 @@ window.onclick = function(event) {
 }
 
 $(document).ready(function() {
-  openMenu(event, 'Burgers');
+  openMenu(null, 'Burgers');
 
-  // document.getElementsById("default").trigger('click');
   $("#loginform").submit(function(event) {
     event.preventDefault();
 
@@ -109,56 +107,56 @@ function logout() {
 }
 
 /*Payment*/
-var owner = $('#owner'),
-  cardNumber = $('#cardNumber'),
-  cardNumberField = $('#card-number-field'),
-  CVV = $("#cvv"),
-  mastercard = $("#mastercard"),
-  confirmButton = $('#confirm-purchase'),
-  visa = $("#visa"),
-  amex = $("#amex");
-cardNumber.payform('formatCardNumber');
-CVV.payform('formatCardCVC');
-
-cardNumber.keyup(function() {
-    amex.removeClass('transparent');
-    visa.removeClass('transparent');
-    mastercard.removeClass('transparent');
-
-    if ($.payform.validateCardNumber(cardNumber.val()) == false) {
-        cardNumberField.removeClass('has-success');
-        cardNumberField.addClass('has-error');
-    } else {
-        cardNumberField.removeClass('has-error');
-        cardNumberField.addClass('has-success');
-    }
-
-    if ($.payform.parseCardType(cardNumber.val()) == 'visa') {
-        mastercard.addClass('transparent');
-        amex.addClass('transparent');
-    } else if ($.payform.parseCardType(cardNumber.val()) == 'amex') {
-        mastercard.addClass('transparent');
-        visa.addClass('transparent');
-    } else if ($.payform.parseCardType(cardNumber.val()) == 'mastercard') {
-        amex.addClass('transparent');
-        visa.addClass('transparent');
-    }
-});
-
-confirmButton.click(function(e) {
-    e.preventDefault();
-
-    var isCardValid = $.payform.validateCardNumber(cardNumber.val());
-    var isCvvValid = $.payform.validateCardCVC(CVV.val());
-
-    if(owner.val().length < 5){
-        alert("Wrong owner name");
-    } else if (!isCardValid) {
-        alert("Wrong card number");
-    } else if (!isCvvValid) {
-        alert("Wrong CVV");
-    } else {
-        // Everything is correct. Add your form submission code here.
-        alert("Everything is correct");
-    }
-});
+// var owner = $('#owner'),
+//   cardNumber = $('#cardNumber'),
+//   cardNumberField = $('#card-number-field'),
+//   CVV = $("#cvv"),
+//   mastercard = $("#mastercard"),
+//   confirmButton = $('#confirm-purchase'),
+//   visa = $("#visa"),
+//   amex = $("#amex");
+// cardNumber.payform('formatCardNumber');
+// CVV.payform('formatCardCVC');
+//
+// cardNumber.keyup(function() {
+//     amex.removeClass('transparent');
+//     visa.removeClass('transparent');
+//     mastercard.removeClass('transparent');
+//
+//     if ($.payform.validateCardNumber(cardNumber.val()) == false) {
+//         cardNumberField.removeClass('has-success');
+//         cardNumberField.addClass('has-error');
+//     } else {
+//         cardNumberField.removeClass('has-error');
+//         cardNumberField.addClass('has-success');
+//     }
+//
+//     if ($.payform.parseCardType(cardNumber.val()) == 'visa') {
+//         mastercard.addClass('transparent');
+//         amex.addClass('transparent');
+//     } else if ($.payform.parseCardType(cardNumber.val()) == 'amex') {
+//         mastercard.addClass('transparent');
+//         visa.addClass('transparent');
+//     } else if ($.payform.parseCardType(cardNumber.val()) == 'mastercard') {
+//         amex.addClass('transparent');
+//         visa.addClass('transparent');
+//     }
+// });
+//
+// confirmButton.click(function(e) {
+//     e.preventDefault();
+//
+//     var isCardValid = $.payform.validateCardNumber(cardNumber.val());
+//     var isCvvValid = $.payform.validateCardCVC(CVV.val());
+//
+//     if(owner.val().length < 5){
+//         alert("Wrong owner name");
+//     } else if (!isCardValid) {
+//         alert("Wrong card number");
+//     } else if (!isCvvValid) {
+//         alert("Wrong CVV");
+//     } else {
+//         // Everything is correct. Add your form submission code here.
+//         alert("Everything is correct");
+//     }
+// });
